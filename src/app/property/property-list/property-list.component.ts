10d59b94c7e3393021b6112dcc2f96e6d265c40b
yeahia khan <yeahia@kdstextile.net>
2022-12-29 17:50:34 +0600

    Using Service and HTTP calls (10 video)

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{ HousingService} from 'src/app/services/housing.service'
 
 @Component({
   selector: 'app-property-list',
@@ -6,47 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<any> = [
-    {
-      Id: 1,
-      Name: 'Birla House',
-      Type: 'House',
-      Price: 12000,
-    },
-    {
-      Id: 1,
-      Name: 'King House',
-      Type: 'House',
-      Price: 10000,
-    },
-    {
-      Id: 1,
-      Name: 'Queen House',
-      Type: 'House',
-      Price: 8000,
-    },
-    {
-      Id: 1,
-      Name: 'Pet House',
-      Type: 'House',
-      Price: 300,
-    },
-    {
-      Id: 1,
-      Name: 'Cat House',
-      Type: 'House',
-      Price: 900,
-    },
-    {
-      Id: 1,
-      Name: 'Dear House',
-      Type: 'House',
-      Price: 900,
-    },
+  properties:any;
 
-  ];
+  constructor(private houseingService:HousingService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.houseingService.getAllProperty().subscribe(
+      data=>{this.properties=data;
+      console.log(data)
+      },error=>{
+        console.log("list not found")
+      }
+      );
+  }
 }
